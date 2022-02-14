fc9ddf0fb20191c637336e899449323f603a3992
Mirkofilip <mirko.filippelli@gmail.com>
2022-02-14 16:02:51 +0100

    add test 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>test</h2>
+    <div class="asd">
+      dasd
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'test',
   components: {
